2f46fc67665541e2129958fcbc289f892d1dbb9f
Krokosha <56129242+MeSmallMan@users.noreply.github.com>
2024-09-11 10:31:50 +0300

    Create allora2.sh

---
+++ allora2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/MeSmallMan/logo/main/logo0.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+echo "-----------------------------------------------------------------------------"
+echo "Установка Allora Worker"
+echo "-----------------------------------------------------------------------------"
+
+echo "Введите сид фразу от кошелька, который будет использоваться для воркера "
+read WALLET_SEED_PHRASE
+
+echo "Введите Coin Gecko API key"
+read COIN_GECKO_API_KEY
+
+cd $HOME
+git clone https://github.com/allora-network/allora-huggingface-walkthrough
+cd allora-huggingface-walkthrough
+mkdir -p worker-data
+chmod -R 777 worker-data
+
+wget https://raw.githubusercontent.com/MeSmallMan/allora/main/config.json
+sed -i "s|SeedPhrase|$WALLET_SEED_PHRASE|" $HOME/allora-huggingface-walkthrough/config.json
+sed -i "s|\":8000|\":18000|" $HOME/allora-huggingface-walkthrough/config.json
+
+sed -i "s|<Your Coingecko API key>|$COIN_GECKO_API_KEY|" $HOME/allora-huggingface-walkthrough/app.py
+
+chmod +x init.config
+./init.config
+
+sed -i "s|\"8000:8000\"|\"18000:8000\"|" $HOME/allora-huggingface-walkthrough/docker-compose.yaml
+docker compose up -d --build
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/MeSmallMan/logo/main/logo0.sh | bash
+echo "-----------------------------------------------------------------------------"
